79f8868f3ab815ab98e68d0c692001c7ca744cef
Noha Ahmed <58637230+noha-ahmed@users.noreply.github.com>
2022-06-03 14:35:54 +0200

    Time: 275 ms (16.80%), Space: 125.1 MB (70.26%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -0,0 +1,27 @@
+class NumMatrix {
+    private int[][] prefixSum;
+
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        prefixSum = new int[n+1][m+1];
+        
+        for(int col = m - 1; col >= 0; col--)
+            for(int row = n - 1; row >= 0; row--)
+                prefixSum[row][col] = matrix[row][col] + prefixSum[row][col+1]
+                    + prefixSum[row+1][col] - prefixSum[row+1][col+1];
+               
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefixSum[row1][col1] - prefixSum[row1][col2+1] 
+            - prefixSum[row2+1][col1] + prefixSum[row2 + 1][col2 + 1];
+        
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
