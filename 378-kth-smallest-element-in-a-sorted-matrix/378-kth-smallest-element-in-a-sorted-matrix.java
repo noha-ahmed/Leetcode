f44102016670e574a133e8ead9419029691ab5b1
Noha Ahmed <58637230+noha-ahmed@users.noreply.github.com>
2022-08-02 23:15:32 +0200

    Time: 28 ms (38.33%), Space: 57.2 MB (39.62%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -4,8 +4,8 @@ class Solution {
     int flag = 1;
     for(int i = 0; i < matrix.length; i++){
         for(int j=0; j < matrix[0].length; j++){
-            
-            pq.add(matrix[i][j]);
+            if(pq.size() < k || pq.peek() > matrix[i][j] )
+                pq.add(matrix[i][j]);
             if(pq.size() > k){
                 pq.poll();
             }
