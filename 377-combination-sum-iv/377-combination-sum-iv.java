5110c16c35d675771de73a2657b297f9138fa858
Noha Ahmed <58637230+noha-ahmed@users.noreply.github.com>
2022-08-06 02:02:17 +0200

    Time: 0 ms (100.00%), Space: 42.3 MB (10.56%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,26 +1,20 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-       HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int num : nums)
-            map.put(num, -1);
-        
-        map.put(0, 1);
-        return countCombRec(nums, map, target);
+       int[] mem = new int[target + 1];
+        Arrays.fill(mem, -1);
+        mem[0] = 1;
+        return countCombRec(nums, mem, target);
     }
     
-    private int countCombRec(int[] nums, HashMap<Integer, Integer> map, int target){
+    private int countCombRec(int[] nums, int[] mem, int target){
         int count = 0;
         
         for(int num : nums){
             if( num <= target ){
                 int temp = target - num;
-                int tempCount = map.getOrDefault(temp, -1);
-                if( tempCount == - 1 ){
-                    tempCount = countCombRec(nums, map, temp);
-                    map.put(temp, tempCount);
-                }
-                count += tempCount;
+                if( mem[temp] == - 1 )
+                    mem[temp] = countCombRec(nums, mem, temp);                
+                count += mem[temp];
             }
         }
         
