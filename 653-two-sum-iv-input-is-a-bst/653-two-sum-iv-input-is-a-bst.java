d4ab979bde3843d295ff3438273bc838e5b29d05
Noha Ahmed <58637230+noha-ahmed@users.noreply.github.com>
2022-10-09 22:59:10 +0200

    Time: 12 ms (14.22%), Space: 54.8 MB (5.17%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -15,21 +15,18 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        ArrayList<Integer> nums = new ArrayList<>();
+        HashSet<Integer> nums = new HashSet<>();
         fillNums(root, nums);
         
-        int l = 0, r = nums.size() - 1;
-        while(l < r){
-            int sum = nums.get(r) + nums.get(l);
-            if(sum == k) return true;
-            if(sum < k) l++;
-            else r--;
+        if(k%2  == 0 ) nums.remove(k/2);
+        for(int num: nums){
+            if(nums.contains(k - num)) return true;
         }
         
         return false;
     }
     
-    private void fillNums(TreeNode root, List<Integer> nums){
+    private void fillNums(TreeNode root, Set<Integer> nums){
         if(root == null) return;
         
         fillNums(root.left, nums);
