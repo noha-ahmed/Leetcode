76c0852609da81d1a05d8d427cb38e5b43e5f170
Noha Ahmed <58637230+noha-ahmed@users.noreply.github.com>
2022-10-18 19:18:31 +0200

    Time: 9 ms (56.22%), Space: 41.4 MB (86.39%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        if( n == 1 )
+            return "1";
+        String prevDigitString = countAndSay(n - 1);
+        return say(prevDigitString);
+    }
+    
+    private String say(String digitString){
+        StringBuilder strBuilder = new StringBuilder();
+        char currentDigit = digitString.charAt(0);
+        int count = 0;
+        for(char d : digitString.toCharArray()){
+            if(currentDigit == d)
+                count++;
+            else{
+                strBuilder.append(count);
+                strBuilder.append(currentDigit);
+                currentDigit = d; count = 1;
+            }
+        }
+        strBuilder.append(count);
+        strBuilder.append(currentDigit);
+        return strBuilder.toString();
+    }
+}
